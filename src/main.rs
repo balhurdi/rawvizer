08420164b0adc4961092c5b75f6653d17e4405d4
Francisco Torrinha <ftorrinha.dev@gmail.com>
2026-05-15 18:35:27 +0100

    feat: Added basic cmd args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, Parser, ValueEnum, Clone, Copy)]
+enum ImageFormat {
+    RGBA8,
+}
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+    #[arg(long)]
+    width: u16,
+    #[arg(long)]
+    height: u16,
+    #[arg(long)]
+    format: ImageFormat,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let conf = viuer::Config {
+        x: 0,
+        y: 0,
+        ..Default::default()
+    };
+
+    let img = image::DynamicImage::ImageRgba8(image::RgbaImage::new(20, 10));
+    viuer::print(&img, &conf).expect("Image printing failed.");
 }
