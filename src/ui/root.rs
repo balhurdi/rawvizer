c8cad429a17142c0cb290d6350ee8dd510fd14f2
Francisco Torrinha <ftorrinha.dev@gmail.com>
2026-05-16 16:42:37 +0100

    feat: Scafold for main page

--- src/ui/root.rs
+++ src/ui/root.rs
@@ -1,7 +1,6 @@
 use ratatui::{
-    layout::Alignment,
-    style::{Color, Stylize},
-    widgets::{Block, BorderType, Paragraph, Widget},
+    layout::{Alignment, Constraint, Layout},
+    widgets::{Block, BorderType, Widget},
 };
 
 use crate::app::App;
@@ -12,23 +11,30 @@ impl Widget for &App {
         Self: Sized,
     {
         let block = Block::bordered()
-            .title("{{project-name}}")
+            .title("Raw Vis")
             .title_alignment(Alignment::Center)
             .border_type(BorderType::Rounded);
 
-        let text = format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                ",
-        );
+        let layout = Layout::vertical([Constraint::Percentage(90), Constraint::Percentage(10)]);
+        let [video, controls] = area.layout(&layout);
 
-        let paragraph = Paragraph::new(text)
-            .block(block)
-            .fg(Color::Cyan)
-            .bg(Color::Black)
-            .centered();
+        let controls_layout = Layout::horizontal([
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+        ]);
 
-        paragraph.render(area, buf);
+        let [file_name, playback_status, current_frame] = controls.layout(&controls_layout);
+
+        let video_block = Block::bordered();
+        let file_name_block = Block::bordered();
+        let playback_status_block = Block::bordered();
+        let current_frame_block = Block::bordered();
+
+        block.render(area, buf);
+        video_block.render(video, buf);
+        file_name_block.render(file_name, buf);
+        playback_status_block.render(playback_status, buf);
+        current_frame_block.render(current_frame, buf);
     }
 }
